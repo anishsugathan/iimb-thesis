0e87162f0ca6d1b10b6d29db37fe6d5bbb56faf3
Anish Sugathan <anish.iimb@gmail.com>
2013-09-19 17:04:19 -0400

    Anish: First Working Commit

---
+++ CompilePDF.bat
@@ -0,0 +1,8 @@
+pdflatex IIMBThesis.tex
+bibtex introduction.aux
+bibtex chapter01.aux
+bibtex chapter02.aux
+bibtex chapter03.aux
+bibtex appendixA.aux
+pdflatex IIMBThesis.tex
+pdflatex IIMBThesis.tex
